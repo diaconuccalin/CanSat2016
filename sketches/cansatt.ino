81fa8f977d835512e4ec7078f302254b765ebf36
maximunus <calin_baracuda2010@yahoo.com>
2016-04-10 15:33:58 +0300

    sketchuri bune dar nefunctionale baza, emisie si geiger + update librarii

---
+++ sketches/cansatt.ino
@@ -0,0 +1,36 @@
+#include <qbcan.h>
+#include <Wire.h>
+#include <SPI.h>
+
+//Radio Parameters
+#define NODEID        2    //unique for each node on same network
+#define NETWORKID     2  //the same on all nodes that talk to each other
+#define GATEWAYID     1    //Receiving node
+#define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
+
+//Radio object
+char payload[50];
+RFM69 radio;
+
+void setup()
+{
+  //Initialize serial connection for debugging
+  Serial.begin(9600);
+  Serial.println("REBOOT");
+
+  //  Initialize radio
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  radio.setHighPower(); //To use the high power capabilities of the RFM69HW
+  radio.encrypt(ENCRYPTKEY);
+  Serial.println("Transmitting at 433 Mhz");
+
+}
+
+void loop()
+{
+  sprintf(payload, "test");
+  radio.send(GATEWAYID, payload, 15);
+  Serial.println("Send complete");
+  
+  delay(500);
+}
---
+++ sketches/geigerr.ino
@@ -0,0 +1,48 @@
+// Conversion factor - CPM to uSV/h
+#define CONV_FACTOR 0.00812
+
+// Variables
+int geiger_input = 6;
+long count = 0;
+long countPerMinute = 0;
+long timePrevious = 0;
+long timePreviousMeassure = 0;
+long time = 0;
+long countPrevious = 0;
+float radiationValue = 0.0;
+
+void setup()
+{
+  pinMode(geiger_input, INPUT);
+  digitalWrite(geiger_input,HIGH);
+  
+  Serial.begin(9600);
+
+  attachInterrupt(digitalPinToInterrupt(2),countPulse,FALLING);
+
+}
+
+void loop()
+{
+  if (millis()-timePreviousMeassure > 10000)
+  {
+    countPerMinute = 6*count;
+    radiationValue = countPerMinute * CONV_FACTOR;
+    timePreviousMeassure = millis();
+    Serial.print("cpm = ");
+    Serial.print(countPerMinute,DEC);
+    Serial.print(" - ");
+    Serial.print("uSv/h = ");
+    Serial.println(radiationValue,4);
+    
+    count = 0;
+  }
+}
+
+void countPulse()
+{
+  detachInterrupt(digitalPinToInterrupt(2));
+  count++;
+  while(digitalRead(2)==0){}
+  attachInterrupt(digitalPinToInterrupt(2),countPulse,FALLING);
+}
